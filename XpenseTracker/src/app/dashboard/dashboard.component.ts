c5e7ef171672505dd3259f518f22300f98b47504
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-08-15 22:18:39 +0200

    added standards to global css variables (temporary solution)

--- XpenseTracker/src/app/dashboard/dashboard.component.ts
+++ XpenseTracker/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
+  styles: [
+    
+  ],
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
