6c346cb48f34da2873112789f745070acb3f55a2
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-08-15 19:56:14 +0200

    Added Sidebar with Hover Effect (html + css)

--- XpenseTracker/src/app/app.module.ts
+++ XpenseTracker/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
