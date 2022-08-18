5a21969982f279afb3c0f0654e12d4672fe8b1f8
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-08-18 23:12:05 +0200

    created standard css and added categories routing

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css', '../general-css/app.media.css']
+  styleUrls: ['./categories.component.css', '../general-css/app.media.css', '../general-css/app.standard.css']
 })
 export class CategoriesComponent implements OnInit {
 
