ccf43a856011ebafdb863195225c9dcdeccfcc21
CanadaCommits <82830595+CanadaCommits@users.noreply.github.com>
2022-08-21 21:31:25 +0200

    cards refactored

---
+++ src/app/components/insights/insights.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insights',
+  templateUrl: './insights.component.html',
+  styleUrls: ['./../dashboard/dashboard.component.css']
+})
+export class InsightsComponent implements OnInit {
+
+  cards: any[] = [];
+
+  constructor() {
+    this.cards = [
+      {
+        title: "Total Income",
+        icon: "analytics",
+        value: "2.676",
+        class: "sales",
+        link: "income"
+      },
+
+      {
+        title: "Total Expenses",
+        icon: "bar_chart",
+        value: "276",
+        class: "expenses",
+        link: "dashboard"
+      },
+
+      {
+        title: "Balance",
+        icon: "stacked_line_chart",
+        value: "51.098",
+        class: "income",
+        link: "dashboard"
+      }
+    ]
+   }
+
+  ngOnInit(): void {
+  }
+
+}
