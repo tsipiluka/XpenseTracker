432ec876490cbb42ee67d542b9d9a7fb26ac95a4
tsipiluka <72984318+tsipiluka@users.noreply.github.com>
2022-09-02 15:16:10 +0200

    add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CategoriesComponent } from './components/categories/categories.componen
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ExpensesComponent } from './components/expenses/expenses.component';
 import { IncomeComponent } from './components/income/income.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'income', component: IncomeComponent},
   { path: 'expenses', component: ExpensesComponent},
   { path: 'categories', component: CategoriesComponent},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
